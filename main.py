2391077a18b8e06b7a195d148447dfff4bb50127
Bruce Hirwa <hirwabruce00@gmail.com>
2026-09-25 16:49:11 +0200

    Small changes

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from operations import np
 print("Welcome to the Vector Algebra Calculator!")
 a = np.array([float(x) for x in input("Enter the first vector (comma-separated values): ").split(',')])
 b = np.array([float(x) for x in input("Enter the second vector (comma-separated values): ").split(',')])
-c = add(a, b)
-print("The result of adding the vectors is:", c)
+addition= add(a,b)
+print("The result of adding the vectors is:", addition)
