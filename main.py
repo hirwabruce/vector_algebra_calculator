41421fc6b2f86d88c84e24ddca40e5e5dcc72e7c
Bruce Hirwa <hirwabruce00@gmail.com>
2026-09-25 16:37:23 +0200

    Addition testing

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from operations import add
+from operations import np
 
 print("Welcome to the Vector Algebra Calculator!")
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+c = add(a, b)
+print("The result of adding the vectors is:", c)
