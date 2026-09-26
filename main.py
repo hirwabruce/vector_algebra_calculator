9e372e6610ac7d71f5b64e16403ee2c94c9e8e5b
Bruce Hirwa <hirwabruce00@gmail.com>
2026-09-26 10:49:24 +0200

    Dot product added in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from operations import add, subtract
+from operations import add, subtract, dot_product
 from operations import np
 
 print("Welcome to the Vector Algebra Calculator!")
@@ -8,3 +8,5 @@ addition= add(a,b)
 print("The result of adding the vectors is:", addition)
 subtraction = subtract(a,b)
 print("The result of subtracting the vectors is:", subtraction)
+dot_product_result = dot_product(a,b)
+print("The result of the dot product of the vectors is:", dot_product_result)
